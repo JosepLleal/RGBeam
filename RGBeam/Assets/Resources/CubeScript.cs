1a862ead6234af4dd80fe69faad035de45dd5d62
Josep Lleal <36154137+JosepLleal@users.noreply.github.com>
2020-05-17 21:09:49 +0200

    Laser first mechanic yay

---
+++ RGBeam/Assets/Resources/CubeScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+
+    public bool LaserHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(LaserHit == true)
+        {
+            if(gameObject.transform.childCount > 0)
+                gameObject.transform.GetChild(0).gameObject.SetActive(true);
+
+            LaserHit = false;
+        }
+        else
+        {
+            if (gameObject.transform.childCount > 0)
+                gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
---
+++ RGBeam/Assets/Resources/Laser.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Laser : MonoBehaviour
+{
+
+    private LineRenderer lr;
+
+    private GameObject collision;
+
+    // Use this for initialization
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lr.SetPosition(0, transform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider)
+            {
+                lr.SetPosition(1, hit.point);
+
+                collision = hit.collider.gameObject;
+
+                if (hit.collider.gameObject.GetComponent<CubeScript>())
+                    hit.collider.gameObject.GetComponent<CubeScript>().LaserHit = true;
+
+            }
+            
+        }
+        else
+        {
+            lr.SetPosition(1, transform.forward * 5000);
+
+           
+        }
+           
+    }
+}
